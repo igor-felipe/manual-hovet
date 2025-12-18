862bd8fc76582bb9d1c2cbae4059dcd8f9111871
Igor Felipe <igor7@outlook.com>
2025-12-18 06:42:17 -0300

    feat: add página home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+import Home from "./home.mdx";
+
 export default function Page() {
   return (
-    <div className="flex h-full items-center justify-center ">
-      <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-        Em construção
-      </h1>
-    </div>
+    <article className="prose container max-w-5xl p-12 my-4 md:px-20 text-justify bg-white">
+      <Home />
+    </article>
   );
 }
