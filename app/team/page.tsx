c6ff91a9bcead1bce34a754d2120dfe2fda019c6
ThomasLemos <72039433+ThomasLemos@users.noreply.github.com>
2026-03-24 17:25:49 -0300

    fix: corrige alinhamento em team-card (#7)

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -4,7 +4,7 @@ import { teamMembers } from '@/lib/data/team-members';
 
 export default function TeamPage() {
   return (
-    <ContentShell className="py-2">
+    <ContentShell className="py-12">
       <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
         {teamMembers.map((member, index) => (
           <TeamCard key={index} member={member} />
--- components/team/team-card.tsx
+++ components/team/team-card.tsx
@@ -9,35 +9,35 @@ interface TeamCardProps {
 export function TeamCard({ member }: TeamCardProps) {
   return (
     <div className="w-full max-w-sm  border bg-card text-card-foreground shadow-sm transition-all hover:shadow-lg">
-      <div className="flex flex-col items-center p-6 text-center">
+      <div className="flex flex-col items-center p-4 text-center">
         <div className="mb-4">
           {member.githubUrl + ".png" ? (
             <img
               src={member.avatarUrl ?? member.githubUrl + ".png"}
               alt={member.name}
-              className="h-24 w-24 rounded-full border-4 border-white shadow-md"
+              className="h-24 w-24 rounded-full object-cover translate-y-2 border-4 border-white shadow-md"
             />
           ) : (
-            <div className="flex h-24 w-24 items-center justify-center rounded-full bg-muted">
+            <div className="flex h-24 w-24 items-center justify-center rounded-full bg-muted translate-y-8">
               <Github className="h-12 w-12 text-muted-foreground" />
             </div>
           )}
         </div>
-        <h3 className="text-lg font-semibold">{member.name}</h3>
+        <h3 className="text-lg font-semibold -mt-1">{member.name}</h3>
       </div>
       
-      {member.bio && (
-        <div className="px-6 pb-4">
-          <p className="text-center text-sm leading-6">{member.bio}</p>
-        </div>
-      )}
+      <div className="px-6 pb-2 h-[64px] -mt-5">
+        <p className="text-center text-sm leading-6 line-clamp-3">
+          {member.bio || ""}
+        </p>
+      </div>
       
-      <div className="flex justify-center p-6 pt-0">
+      <div className="flex justify-center p-6 pt-0 mt-auto">
         <Link
           href={member.githubUrl}
           target="_blank"
           rel="noopener noreferrer"
-          className="flex items-center gap-2  bg-primary px-4 py-2 text-sm font-medium text-primary-foreground hover:bg-primary/90"
+          className="flex items-center gap-2 bg-primary px-4 py-2 text-sm font-medium text-primary-foreground hover:bg-primary/90"
         >
           <Github className="h-4 w-4" />
           Ver perfil no GitHub
